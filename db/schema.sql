23762d10ebc347df4b4da5b49456ed7c0dd8f408
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2022-06-28 22:41:29 -0700

    functions working in js, updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,28 @@
-
-
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db; 
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30),
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30), 
-    salary DECIMAL (8, 2),
-    department_id INT, 
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL, 
+    salary INTEGER NOT NULL,
+    department_id INTEGER, 
     INDEX dep_ind (department_id),
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
-    
+   FOREIGN KEY (department_id) REFERENCES department(id) 
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT, 
+    role_id INTEGER, 
     INDEX role_ind (role_id),
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    manager_id INT,
-    INDEX manager_ind (manager_id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+   FOREIGN KEY (role_id) REFERENCES role(id) ,
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) 
 );
